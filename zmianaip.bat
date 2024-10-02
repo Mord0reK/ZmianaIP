9ddb9367509fbc48be4dfad7834e4ef89c7dff4b
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-10-02 19:58:22 +0200

    Fix

--- zmianaip.bat
+++ zmianaip.bat
@@ -14,12 +14,23 @@ netsh interface show interface
 pause
 
 set /p nazwa="Podaj nazwe interfejsu: "
-set /p nowy_ip="Podaj nowy adres IP: "
-set /p maska="Podaj maske podsieci: "
-set /p brama="Podaj brame domyslna: "
+set /p tryb="Czy chcesz ustawic statyczny adres IP (s) czy przywrocic ustawienia DHCP (d)? [s/d]: "
 
-rem Zmiana adresu IP
-netsh interface ipv4 set address name=%nazwa% static %nowy_ip% %maska% %brama%  
+if /i "%tryb%"=="s" (
+	set /p nowy_ip="Podaj nowy adres IP: "
+	set /p maska="Podaj maske podsieci: "
+	set /p brama="Podaj brame domyslna: "
+
+	rem Zmiana adresu IP na statyczny
+	netsh interface ipv4 set address name=%nazwa% static %nowy_ip% %maska% %brama%  
+) else if /i "%tryb%"=="d" (
+	rem Przywrócenie ustawień DHCP
+	netsh interface ipv4 set address name=%nazwa% source=dhcp
+	netsh interface ipv4 set dns name=%nazwa% source=dhcp
+) else (
+	echo Nieprawidlowy wybor.
+	exit /b
+)
 
 rem Sprawdzenie nowej konfiguracji
 netsh interface ipv4 show config name="%nazwa%"
