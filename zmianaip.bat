89c2ed52de30d21904ff21d564a21f08c28fba9b
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-10-02 19:45:59 +0200

    Utworzenie pliku

---
+++ zmianaip.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set /p nazwa = "Podaj nazwe interfejsu: "
+set /p nowy_ip = "Podaj nowy adres IP: "
+set /p maska = "Podaj maske podsieci: "
+set /p brama = "Podaj brame domyslna: "
+
+rem Zmiana adresu IP
+netsh interface ip set address name="%nazwa%" static %nowy_ip% %maska% %brama%    
+
+rem Sprawdzenie nowej konfiguracji
+netsh interface ip show config name="%interface_name%"
+
+pause
