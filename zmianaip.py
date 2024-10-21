2dad3a0ae4f9224a19b568d6f4dbb1a8f67f5b48
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-10-21 19:32:20 +0200

    Dodanie Gateway, DHCP, ipconfig renew/release, arp'a

--- zmianaip.py
+++ zmianaip.py
@@ -86,51 +86,149 @@ def map_interface_to_standard_name(interface):
 def change_interface_params(interface, ip, netmask, gateway, dns=None):
     standardized_interface = map_interface_to_standard_name(interface)
 
-    command = f'netsh interface ip set address name="{standardized_interface}" static {ip} {netmask} {gateway}'
-    print(f"Wykonywanie komendy: {command}")
-    result = os.system(command)
+    if ip:  # If the user provided a new IP
+        command = f'netsh interface ip set address name="{standardized_interface}" static {ip} {netmask} {gateway}'
+        print(f"Wykonywanie komendy: {command}")
+        result = os.system(command)
+
+        if result == 0:
+            print(f"Poprawnie zaaktualizowano interfejs '{standardized_interface}' z Adresem IP {ip}, Maską {netmask}, oraz Bramą {gateway}.")
+        else:
+            print(f"Nie udało się zmienić parametrów interfejsu '{standardized_interface}'. Sprawdź parametry i spróbuj ponownie.")
+
+        if dns:
+            dns_command = f'netsh interface ip set dns name="{standardized_interface}" static {dns}'
+            print(f"Wykonywanie komendy DNS: {dns_command}")
+            dns_result = os.system(dns_command)
+
+            if dns_result == 0:
+                print(f"Poprawnie zaaktualizowano DNS dla interfejsu '{standardized_interface}' na {dns}.")
+            else:
+                print(f"Nie udało się zmienić serwera DNS dla interfejsu '{standardized_interface}'. Sprawdź parametr DNS i spróbuj ponownie.")
+    else:  # If no IP was provided, switch to DHCP
+        print(f"Ustawianie interfejsu '{standardized_interface}' na DHCP.")
+        dhcp_command = f'netsh interface ip set address name="{standardized_interface}" source=dhcp'
+        result = os.system(dhcp_command)
+
+        if result == 0:
+            print(f"Interfejs '{standardized_interface}' został ustawiony na pobieranie adresu IP z DHCP.")
+        else:
+            print(f"Nie udało się ustawić interfejsu '{standardized_interface}' na DHCP. Sprawdź parametry i spróbuj ponownie.")
+
+        if dns:
+            dns_command = f'netsh interface ip set dns name="{standardized_interface}" source=dhcp'
+            print(f"Wykonywanie komendy DNS: {dns_command}")
+            dns_result = os.system(dns_command)
+
+            if dns_result == 0:
+                print(f"Poprawnie zaaktualizowano DNS dla interfejsu '{standardized_interface}' na DHCP.")
+            else:
+                print(f"Nie udało się zmienić serwera DNS dla interfejsu '{standardized_interface}'. Sprawdź parametr DNS i spróbuj ponownie.")
+
+
+def release_and_renew_ip():
+    # Ustaw wszystkie interfejsy na DHCP
+    interface_list = list_interfaces()  # Zaktualizuj dane interfejsów
+    for interface in interface_list:
+        dhcp(interface)  # Ustaw każdy interfejs na DHCP
+
+    print("Wykonywanie komendy: ipconfig /release")
+    os.system('ipconfig /release')
+    print("Wykonywanie komendy: ipconfig /renew")
+    os.system('ipconfig /renew')
+
+
+
+def display_arp_table():
+    print("Wykonywanie komendy: arp -a")
+    os.system('arp -a')
+    os.system('pause')
+
+def dhcp(interface):
+    standardized_interface = map_interface_to_standard_name(interface)
+    dhcp_command = f'netsh interface ip set address name="{standardized_interface}" source=dhcp'
+    result = os.system(dhcp_command)
 
     if result == 0:
-        print(f"Poprawnie zaaktualizowano interfejs '{standardized_interface}' z Adresem IP {ip}, Maską {netmask}, oraz Bramą {gateway}.")
+        print(f"Interfejs '{standardized_interface}' został ustawiony na pobieranie adresu IP z DHCP.")
+    elif result == "DHCP is already enabled on this interface.".upper():
+        print(f"Interfejs '{standardized_interface}' jest już skonfigurowany na DHCP.")
     else:
-        print(f"Nie udało się zmienić parametrów interfejsu '{standardized_interface}'. Sprawdź parametry i spróbuj ponownie.")
+        print(f"Nie udało się ustawić interfejsu '{standardized_interface}' na DHCP. Sprawdź parametry i spróbuj ponownie.")
 
-    if dns:
-        dns_command = f'netsh interface ip set dns name="{standardized_interface}" static {dns}'
-        print(f"Wykonywanie komendy DNS: {dns_command}")
-        dns_result = os.system(dns_command)
+    dns_command = f'netsh interface ip set dns name="{standardized_interface}" source=dhcp'
+    print(f"Wykonywanie komendy DNS: {dns_command}")
+    dns_result = os.system(dns_command)
 
-        if dns_result == 0:
-            print(f"Poprawnie zaaktualizowano DNS dla interfejsu '{standardized_interface}' na {dns}.")
-        else:
-            print(f"Nie udało się zmienić serwera DNS dla interfejsu '{standardized_interface}'. Sprawdź parametr DNS i spróbuj ponownie.")
+    if dns_result == 0:
+        print(f"Poprawnie zaaktualizowano DNS dla interfejsu '{standardized_interface}' na DHCP.")
+    else:
+        print(f"Nie udało się zmienić serwera DNS dla interfejsu '{standardized_interface}'. Sprawdź parametr DNS i spróbuj ponownie.")
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 if __name__ == "__main__":
+    clear_console()  # Opcjonalnie, czyść konsolę na początku
     try:
-        interface_list = list_interfaces()
+        while True:
+            clear_console()  # Czyści konsolę przed wyświetleniem menu
+            interface_list = list_interfaces()  # Zaktualizuj dane interfejsów
 
-        if not interface_list:
-            print("Nie znaleziono żadnych interfejsów.")
-            exit(1)
+            if not interface_list:
+                print("Nie znaleziono żadnych interfejsów.")
+                exit(1)
 
-        # Input and validation
-        try:
-            interface_idx = int(input("Wprowadź numer interfejsu, który chcesz zmienić: ")) - 1
-            if interface_idx < 0 or interface_idx >= len(interface_list):
-                raise IndexError("Wybrano niepoprawny interfejs.")
-        except ValueError as e:
-            print(f"Błąd: {e}. Wprowadź prawidłowy numer interfejsu (liczba).")
-            exit(1)
+            # Menu dla użytkownika
+            print("\nWybierz opcje:")
+            print("1) Zmiana parametrów kart sieciowych")
+            print("2) Wyświetlanie parametrów kart sieciowych")
+            print("3) Ipconfig /release & /renew")
+            print("4) arp -a")
+            print("5) Koniec")
+            action_choice = input("Opcja: ").strip()
 
-        interface = interface_list[interface_idx]
-        ip = input("Wprowadź nowy adres IP: ").strip()
-        netmask = input("Wprowadź nową Maskę Podsieci: ").strip()
-        gateway = input("Wprowadź nowy adres bramy: ").strip()
-        dns = input("Wprowadź nowy adres serwera DNS (Zostaw puste, jeżeli nie zmieniasz): ").strip()
+            if action_choice == '1':
+                # Input and validation for changing interface parameters
+                try:
+                    interface_idx = int(input("Wprowadź numer interfejsu, który chcesz zmienić: ")) - 1
+                    if interface_idx < 0 or interface_idx >= len(interface_list):
+                        raise IndexError("Wybrano niepoprawny interfejs.")
+                except ValueError as e:
+                    print(f"Błąd: {e}. Wprowadź prawidłowy numer interfejsu (liczba).")
+                    continue
 
-        # Apply the changes
-        change_interface_params(interface, ip, netmask, gateway, dns if dns else None)
+                interface = interface_list[interface_idx]
+                ip = input("Wprowadź nowe IP (zostaw puste, aby ustawić DHCP): ").strip()
+                if not ip:
+                    dhcp(interface)
+                    continue  # Zmień 'break' na 'continue' tutaj, aby wrócić do menu
+                netmask = input("Wprowadź nową Maskę Podsieci: ").strip()
+                gateway = input("Wprowadź nową Bramę: ").strip()
+                dns = input("Wprowadź nowy DNS (Zostaw puste, jeżeli nie zmieniasz): ").strip()
+
+                # Apply the changes
+                change_interface_params(interface, ip if ip else None, netmask, gateway, dns if dns else None)
+
+            elif action_choice == '2':
+                # Display interface parameters
+                list_interfaces()
+
+            elif action_choice == '3':
+                # Execute the release and renew commands
+                release_and_renew_ip()
+
+            elif action_choice == '4':
+                # Display ARP table
+                display_arp_table()
+
+            elif action_choice == '5':
+                print("Jurecki Babka.")
+                break
+
+            else:
+                print("Niepoprawny wybór. Proszę wprowadzić numer opcji od 1 do 5.")
 
     except KeyboardInterrupt:
         print("\nOperacja anulowana przez użytkownika.")
@@ -138,3 +236,4 @@ if __name__ == "__main__":
         print(f"Błąd: {e}. Nieprawidłowy wybór interfejsu. Spróbuj ponownie.")
     except ValueError as e:
         print(f"Błąd: {e}. Wprowadź poprawne wartości.")
+
